0bd446355e06d26cdeed25c45ed8625c34c17136
Gerrit Hall <gerrit@rezscore.com>
2022-10-10 16:19:12 -0700

    init: First commit

---
+++ tests/voting/test_withdraw.py
@@ -0,0 +1,22 @@
+from brownie import chain
+import brownie
+
+def test_cannot_withdraw_prematurely(staked_nft, tetra, thing, npc, holder):
+    assert npc.balanceOf(tetra) == 0
+
+    with brownie.reverts():
+        staked_nft.withdraw({'from': tetra})
+    
+
+def test_withdraw_nft_balance_up(staked_nft, tetra, thing,alice, npc, holder):
+    chain.mine(timedelta=1000)
+    staked_nft.admin_trigger_epoch(True, {'from': alice})
+    thing_init = thing.balanceOf(tetra)
+    assert npc.balanceOf(tetra) == 0
+
+    staked_nft.withdraw({'from': tetra})
+    
+    assert thing.balanceOf(tetra) > thing_init
+    assert npc.balanceOf(tetra) > 0
+
+
